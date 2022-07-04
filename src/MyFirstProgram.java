48afa4d0ce1ee45f0b240d11a955e6a1db471af8
Pali Varga <pavol.varga2@ebcont.com>
2022-07-04 19:40:56 +0200

    MyFirstProgram

--- src/MyFirstProgram.java
+++ src/MyFirstProgram.java
@@ -2,12 +2,14 @@ package src;
 
 public class MyFirstProgram {
     void run() {
-        System.out.println( "Ahoj Rajmond!" );
+        String text1 = "3";
+        String text2 = "4";
+        print( text1 + " .. " + text2 );
 
         int a = 3;
         int b = 4;
-        System.out.println( a + b );
-        System.out.println( a * b );
+        print( a + b );
+        print( a * b );
     }
 
 
@@ -15,8 +17,23 @@ public class MyFirstProgram {
      * Driver
      */
     public static void main(String[] args) {
-        System.out.println();
-
         new MyFirstProgram().run();
     }
+
+    static void print() {
+        System.out.println();
+    }
+
+    static void print(String string) {
+        System.out.println(string);
+    }
+
+    void print(int i) {
+        System.out.println(i);
+    }
+
+    void print(long i) {
+        System.out.println(i);
+    }
+
 }
