56fbf03407db466da994bc303795e49d9d59de64
Rajmond Varga <rajmond.varga.2011@gmail.com>
2022-07-05 11:49:49 +0200

    MyFirstProgram

--- src/MyFirstProgram.java
+++ src/MyFirstProgram.java
@@ -1,9 +1,10 @@
 package src;
 
 public class MyFirstProgram {
-    void run() {
-        String text1 = "3";
-        String text2 = "4";
+
+    static void run() {
+        String text1 = "8";
+        String text2 = "hallo";
         print( text1 + " .. " + text2 );
 
         int a = 3;
@@ -11,4 +12,18 @@ public class MyFirstProgram {
         print( a + b );
         print( a * b );
     }
+
+    public static void main(String[] args) {
+        run();
+    }
+
+    public static void print( int a){
+        System.out.println(a);
+    }
+
+    public static void print (String s){
+        System.out.println(s);
+    }
+
+
 }
