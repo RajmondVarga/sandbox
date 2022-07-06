15136b5502d43d944501e035509dd9b15c264591
Rajmond Varga <rajmond.varga.2011@gmail.com>
2022-07-06 12:28:12 +0200

    Aufgabe

--- src/Aufgabe.java
+++ src/Aufgabe.java
@@ -1,5 +1,28 @@
 package src;
 
 public class Aufgabe {
+    public static void main(String[] args) {
+        zwei();
+        zwei();
+        zwei();
+        summe();
+    }
 
+    static void summe() {
+        int a = 3;
+        int b = 8;
+        int c = 9;
+
+        print(a + b + c);
+    }
+
+    static void zwei(){
+        int a = 3;
+        int k = 12;
+        print(a + k);
+    }
+
+    static void print( int n) {
+        System.out.println(n);
+    }
 }
--- src/MyFirstProgram.java
+++ src/MyFirstProgram.java
@@ -32,5 +32,7 @@ public class MyFirstProgram {
     }
 
 
+
+
 }
 
