fc49bd6b4f5bd58198f5c3ccc064c2641963ef56
Rajmond Varga <rajmond.varga.2011@gmail.com>
2023-02-13 11:29:32 +0100

    City Block

---
+++ src/CityBlock.java
@@ -0,0 +1,52 @@
+package src;
+
+import java.util.List;
+
+public class CityBlock {
+  List<Integer> data = List.of(8, 7, 3, 4);
+  char block = '\u2588';
+  char space = ' ';
+
+  void generateSkyLine() {
+    int aktuell = maxNumber();
+    String row = "";
+
+    while(aktuell > 0) {
+      for(Integer i : data) {
+        if(i < aktuell) {
+          row += space;
+        } else {
+          row += block;
+        }
+      }
+      printRow(row);
+      row = "";
+      aktuell--;
+    }
+  }
+
+  int maxNumber() {
+    int max = 0;
+    for(Integer i : data) {
+
+      if(i > max) {
+        max = i;
+      }
+    }
+    return max;
+  }
+
+  void printRow(String row) {
+    System.out.println(row);
+  }
+
+  void line() {
+    System.out.println();
+  }
+
+  public static void main(String[] args) {
+    CityBlock skyLine = new CityBlock();
+    skyLine.line();
+    skyLine.generateSkyLine();
+  }
+}
